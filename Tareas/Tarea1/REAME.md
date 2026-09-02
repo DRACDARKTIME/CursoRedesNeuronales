7c3d8a45d97a0b2bb75a1ea44a5c824f2d697397
drac <gjose3159@gmail.com>
2026-09-02 16:56:00 -0600

    We trained the net with SGD, we added a picture of the training in README.md, we also change y_train because the funtion cost_derivate, it needed to be in one-hot coding

---
+++ Tareas/Tarea1/REAME.md
@@ -0,0 +1,5 @@
+# README
+## First training
+Screenshot of training with SGD mini-batch using mnist, eta = 0.01, epoch = 30, batch_size = 120, sizes=[784,240,120,10].
+\
+![Training mini-batch SGD](trainingMiniBatchSGD.png)
--- Tareas/Tarea1/training.py
+++ Tareas/Tarea1/training.py
@@ -8,11 +8,17 @@ if __name__=="__main__":
     x_train = x_train.astype(np.float32) / 255.0
     x_test = x_test.astype(np.float32) / 255.0
 
+    num_classes = 10
+    # In the funtion cost_derivative we can see that y_train needs to be in one-hot
+    # but in the funtion evaluate y_test needs to be a constant
+    y_train_one_hot = np.eye(num_classes, dtype=np.float32)[y_train]
+    y_train_one_hot = y_train_one_hot.reshape(y_train_one_hot.shape[0],10,1)
+
     x_train = x_train.reshape(x_train.shape[0],784, 1)
     x_test = x_test.reshape(x_test.shape[0],784, 1)
 
-    Red = Network(sizes=[784,120,10])
-    Red.SGD(training_data=list(zip(x_train, y_train)),
-     epochs=10, mini_batch_size=30,eta=0.01,
+    Red = Network(sizes=[784,240,120,10])
+    Red.SGD(training_data=list(zip(x_train, y_train_one_hot)),
+     epochs=30, mini_batch_size=120,eta=0.01,
      test_data=list(zip(x_test, y_test)))
     
